4fdb8d3d110ce5b35f255d24380c67fbc9648ab0
simxn1 <mcmunka@gmail.com>
2020-12-31 21:09:48 +0100

    added second section for mobile

--- src/App.js
+++ src/App.js
@@ -31,38 +31,31 @@ const App = () => {
               </div>
           </div>
           <div className="section">
-            <img src="/img/mobile-2.png"/>
+            <img src="/img/mobile-2.png" />
             <div className="slide-selectors">
               <button onClick={prevSlide}><i class="fas fa-chevron-left"></i></button>
               <button onClick={nextSlide}><i class="fas fa-chevron-right"></i></button>
             </div>
           </div>
           <div style={{ backgroundImage: `url("/img/mobile-3-4.png")`, backgroundSize: `cover` }} className="section">
-            <div className="slide-selectors">
-              <button onClick={prevSlide}><i class="fas fa-chevron-left"></i></button>
-              <button onClick={nextSlide}><i class="fas fa-chevron-right"></i></button>
-            </div>
+              <img className="section-2-icon" src="/img/cocoahorizons.png" />
+              <h2 className="section-2-heading">we care</h2>
+              <p className="section-2-text">Čokoláda, ktorú používame, pochádza z kakaa získavaného etickým poľnohospodárstvom.</p>
           </div>
-          <div className="section">
-            <img src="/img/mobile-3-4.png"/>
-            <div className="slide-selectors">
-              <button onClick={prevSlide}><i class="fas fa-chevron-left"></i></button>
-              <button onClick={nextSlide}><i class="fas fa-chevron-right"></i></button>
-            </div>
+          <div style={{ backgroundImage: `url("img/mobile-3-4.png")` }} className="section">
+            <img className="section-2-icon" src="/img/earth.png" />
+            <h2 className="section-2-heading">all natural</h2>
+            <p className="section-2-text">Pokiaľ ide o naše ingrediencie, sme veľmi vyberaví. Využívame iba to najlepšie, čo príroda ponúka.</p>
           </div>
-          <div className="section">
-            <img src="/img/mobile-5-6.png"/>
-            <div className="slide-selectors">
-              <button onClick={prevSlide}><i class="fas fa-chevron-left"></i></button>
-              <button onClick={nextSlide}><i class="fas fa-chevron-right"></i></button>
-            </div>
+          <div style={{ backgroundImage: `url("img/mobile-5-6.png")` }} className="section">
+            <img className="section-2-icon" src="/img/noartificials.png" />
+            <h2 className="section-2-heading">no artificials</h2>
+            <p className="section-2-text">Chuť a kvalita sú všetkým. Využívame pomerne veľa času hľadaním skvelých ingrediencií a zaujímavých chutí.</p>
           </div>
-          <div className="section">
-            <img src="/img/mobile-5-6.png"/>
-            <div className="slide-selectors">
-              <button onClick={prevSlide}><i class="fas fa-chevron-left"></i></button>
-              <button onClick={nextSlide}><i class="fas fa-chevron-right"></i></button>
-            </div>
+          <div style={{ backgroundImage: `url("img/mobile-5-6.png")` }} className="section">
+            <img className="section-2-icon" src="/img/glutenfree.png" />
+            <h2 className="section-2-heading">gluten free</h2>
+            <p className="section-2-text">Myslíme na všetkých milovníkov čokolády, a preto je väčšina z našich tyčiniek vhodná aj pre ľudí s alergiou na lepok.</p>
           </div>
         </Carousel>
       </div>
