fcb9a3687b4150ee1ec6bfaefc411d779c279815
simxn1 <mcmunka@gmail.com>
2020-12-31 14:01:36 +0100

    first two sections for mobile w/o text

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './css/App.css';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { Carousel } from 'react-responsive-carousel';
 
-function App() {
+
+const App = () => {
+  
+  const [currentSlide, setCurrentSlide] = React.useState(0)
+
+  const nextSlide = () => {
+    //if (currentSlide < x) {
+      setCurrentSlide(currentSlide + 1)
+    //}
+  }
+
+  const prevSlide = () => {
+    if (currentSlide > 0) {
+      setCurrentSlide(currentSlide - 1)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <Carousel autoPlay={false} selectedItem={currentSlide}>
+          <div className="section">
+            <img src="/img/mobile-1.png"></img>
+            <div className="slide-selectors">
+              <button onClick={prevSlide}><i class="fas fa-chevron-left"></i></button>
+              <button onClick={nextSlide}><i class="fas fa-chevron-right"></i></button>
+              </div>
+          </div>
+          <div className="section">
+            <img src="/img/mobile-2.png"/>
+            <div className="slide-selectors">
+              <button onClick={prevSlide}><i class="fas fa-chevron-left"></i></button>
+              <button onClick={nextSlide}><i class="fas fa-chevron-right"></i></button>
+            </div>
+          </div>
+          <div style={{ backgroundImage: `url("/img/mobile-3-4.png")`, backgroundSize: `cover` }} className="section">
+            <div className="slide-selectors">
+              <button onClick={prevSlide}><i class="fas fa-chevron-left"></i></button>
+              <button onClick={nextSlide}><i class="fas fa-chevron-right"></i></button>
+            </div>
+          </div>
+          <div className="section">
+            <img src="/img/mobile-3-4.png"/>
+            <div className="slide-selectors">
+              <button onClick={prevSlide}><i class="fas fa-chevron-left"></i></button>
+              <button onClick={nextSlide}><i class="fas fa-chevron-right"></i></button>
+            </div>
+          </div>
+          <div className="section">
+            <img src="/img/mobile-5-6.png"/>
+            <div className="slide-selectors">
+              <button onClick={prevSlide}><i class="fas fa-chevron-left"></i></button>
+              <button onClick={nextSlide}><i class="fas fa-chevron-right"></i></button>
+            </div>
+          </div>
+          <div className="section">
+            <img src="/img/mobile-5-6.png"/>
+            <div className="slide-selectors">
+              <button onClick={prevSlide}><i class="fas fa-chevron-left"></i></button>
+              <button onClick={nextSlide}><i class="fas fa-chevron-right"></i></button>
+            </div>
+          </div>
+        </Carousel>
+      </div>
   );
 }
 
