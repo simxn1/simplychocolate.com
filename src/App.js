ce182ecdbd10cee2649e102945d4034326b5d46e
simxn1 <mcmunka@gmail.com>
2021-01-20 20:30:48 +0100

    device-detect

--- src/App.js
+++ src/App.js
@@ -4,15 +4,14 @@ import Intro from "./components/mobile/Intro";
 import About from "./components/mobile/About";
 import Products from "./components/mobile/Products";
 import Contact from './components/mobile/Contact';
-import { MobileView, BrowserView, isMobile, isBrowser } from 'react-device-detect';
-import { Carousel } from 'react-responsive-carousel';
+import { isMobile } from 'react-device-detect';
 import IntroDesktop from './components/desktop/IntroDesktop';
 import AboutDesktop from "./components/desktop/AboutDesktop";
 
 if (isMobile) {
   import('./css/mobileApp.css')
 }
-else if (isBrowser) {
+else {
   import('./css/desktopApp.css')
 }
 
@@ -29,7 +28,7 @@ const App = () => {
       <Contact />
     </div>
   )
-  else if (isBrowser) return (
+  else return (
     <div className="desktop-components-container">
       <IntroDesktop />
       <AboutDesktop />
