3f2af724953f848e2632096c6314b5ced2fd3c9a
simxn1 <mcmunka@gmail.com>
2021-02-12 21:52:07 +0100

    server changed to "back" for production

--- src/config/endpoints.js
+++ src/config/endpoints.js
@@ -2,7 +2,7 @@ const PRODUCTION_MODE = process.env.NODE_ENV === "production";
 
 let SERVER = "http://localhost:9000";
 if (PRODUCTION_MODE) {
-    SERVER = "";
+    SERVER = "back";
     console.log("production");
 }
 else {
