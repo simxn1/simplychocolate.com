9de628700bc3b3c4615638b04d7597d58d1bf271
Šimon Munka <pro@MacBook-Air.local>
2021-02-20 01:02:27 +0100

    about fixes✅

--- src/components/mobile/AboutPage.js
+++ src/components/mobile/AboutPage.js
@@ -3,8 +3,7 @@ import React from 'react';
 const AboutPage = (props) => {
 
     return (
-        <div className="section">
-            <img src={`/img/mobile/mobile-${props.bg}.png`} className="slide-bg" />
+        <div style={{ background: `url(/img/mobile/mobile-${props.bg}.png) no-repeat`  }} className="section">
             <img className="section-2-icon" src={`/img/mobile/${props.icon}.png`} />
             <h2 className="section-2-heading">{props.heading}</h2>
             <p className="section-2-text">{props.desc}</p>
